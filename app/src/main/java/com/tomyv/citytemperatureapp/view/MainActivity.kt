789c11f1bd537f150f297bff58e1a7b56aae4c97
Tomas Vitkus <tomas.vitkus@telesoftas.com>
2021-10-10 19:29:13 +0300

    Update main activity to present time with city temperature

--- app/src/main/java/com/tomyv/citytemperatureapp/view/MainActivity.kt
+++ app/src/main/java/com/tomyv/citytemperatureapp/view/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.tomyv.citytemperatureapp.view
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.tomyv.citytemperatureapp.R
+import com.tomyv.citytemperatureapp.view.viewModel.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showTemperatureInfo()
+
+        val swipeContainer: SwipeRefreshLayout = findViewById(R.id.swipeRefresh)
+        swipeContainer.setOnRefreshListener {
+            showTemperatureInfo()
+            swipeContainer.isRefreshing = false
+        }
+    }
+
+    private fun showTemperatureInfo() {
+        val timeContainer: TextView? = findViewById(R.id.textView)
+        val weatherContainer: TextView? = findViewById(R.id.textView2)
+        val city = getString(R.string.city_name)
+        val mainActivityViewModel: MainActivityViewModel =
+            ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        mainActivityViewModel.getTemperature(city).observe(this,
+            { temperatureInfo -> weatherContainer!!.text = temperatureInfo })
+        mainActivityViewModel.getCurrentTime()
+            .observe(this, { timeInfo -> timeContainer!!.text = timeInfo })
     }
 }
